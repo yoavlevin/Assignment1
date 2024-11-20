b3319d73abc6d4fe1681fbdebb02285ffc8eb491
Yoav Levin <yoavlevi@post.bgu.ac.il>
2024-11-20 09:45:36 +0000

    initialize project

---
+++ makefile
@@ -0,0 +1,2 @@
+# Please implement your Makefile rules and targets below.
+# Customize this file to define how to build your project.
---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+#include "Simulation.h"
+#include <iostream>
+
+using namespace std;
+
+Simulation* backup = nullptr;
+
+int main(int argc, char** argv){
+    if(argc!=2){
+        cout << "usage: simulation <config_path>" << endl;
+        return 0;
+    }
+    string configurationFile = argv[1];
+    Simulation simulation(configurationFile);
+    simulation.start();
+    if(backup!=nullptr){
+    	delete backup;
+    	backup = nullptr;
+    }
+    return 0;
+}
